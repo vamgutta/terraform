b1df12fc2ec9c11081c083412e946f7b4df68a39
vamgutta <vamsi20@gmail.com>
2020-06-02 18:24:25 +0530

    file updated

--- 02-variables/01-var.tf
+++ 02-variables/01-var.tf
@@ -6,10 +6,16 @@ variable "Number" {
   default = 1000
 }
 
+variable "BOOLEAN" {
+  default = true
+}
+
 output "Number" {
   value = var.Number
 }
-
 output "NAME" {
   value = var.NAME
+}
+output "BOOLEAN" {
+  value = var.BOOLEAN
 }
