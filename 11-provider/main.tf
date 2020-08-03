9547ac7af16bd0c5e944632aeb3a626a6ab558aa
vamgutta <vamsi20@gmail.com>
2020-08-03 23:13:13 +0530

    Teraform checking in intillij

--- 11-provider/main.tf
+++ 11-provider/main.tf
@@ -1,3 +1,18 @@
-output "Message" {
-  value = "Sravanthi Budda"
+provider "aws" {
+  region     = "us-west-2"
+}
+resource "aws_vpc" "terra_vpc" {
+  cidr_block       = "162.32.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_subnet" "terra_subnet" {
+  vpc_id     = "${aws_vpc.terra_vpc.id}"
+  cidr_block = "162.32.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
 }
