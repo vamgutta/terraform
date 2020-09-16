0aee877cd9d584aaba5923164eec12491b76879f
vamgutta <vamsi20@gmail.com>
2020-09-16 13:50:16 +0530

    Teraform checking in intillij

--- 02-directory/01-var.tf
+++ 02-directory/01-var.tf
@@ -4,9 +4,15 @@ variable "Name" {
 variable "Number" {
   default = 100
 }
+variable "Boolen" {
+  default = true
+}
 output "Name" {
   value = var.Name
 }
 output "Number" {
   value = var.Number
+}
+output "Boolen" {
+  value = var.Boolen
 }
